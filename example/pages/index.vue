ea2f401a6268499ce3e1c3d3eab1d998d8eb2828
pirony <romainpouchol@gmail.com>
2020-03-21 16:05:49 +0100

    first

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="test" ref="test">
+  <h1 ref="test" class="test">
     Works!
   </h1>
 </template>
@@ -8,9 +8,9 @@
 export default {
   mounted () {
     this.$nextTick(() => {
-      var tl = this.$gsap.timeline({repeat: -1, repeatDelay: 0,  ease: this.$gsap.Linear.easeInOut(2)})
-      tl.to(".test", 2, {x:200}) // With css selector
-      tl.to(this.$refs.test, 0.5, {x: 0}) // With refs
+      const tl = this.$gsap.timeline({ repeat: -1, repeatDelay: 0, ease: this.$gsap.Linear.easeInOut(2) })
+      tl.to('.test', 2, { x: 200 }) // With css selector
+      tl.to(this.$refs.test, 0.5, { x: 0 }) // With refs
     })
   }
 }
